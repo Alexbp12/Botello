51e6ec6ce98eaffb962f5abe0a7205badb552c97
Alexbp12 <141078657+Alexbp12@users.noreply.github.com>
2023-08-02 21:44:51 +0000

    ejercicio 3

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # Primer codigo python
-# Ejercicio 1
-print("Hola Mundo")
+# Ejercicio 1:
+#print("Hola Mundo")
+# Ejercicio 2:
+''''
 Lapicero= 'Hola Mundo'
 print(Lapicero)
+'''
+# Ejercicio 3:
+''''
+nombre= "escribe tu nombre de usuario: "
+mensaje = input (nombre)
+print('¡Hola ' +  mensaje  + '!')
+'''
+# Ejercicio 4"
+
