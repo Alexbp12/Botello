5a635ebb18355f31d3c7d93958ba0b02b60923b8
Alexbp12 <141078657+Alexbp12@users.noreply.github.com>
2023-08-02 22:06:12 +0000

    ejercicio 5

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ nombre= "escribe tu nombre de usuario: "
 mensaje = input (nombre)
 print('¡Hola ' +  mensaje  + '!')
 '''
-# Ejercicio 4"
-
+# Ejercicio 4
+'''
+print(((3+2)/(2*5))**2)
+'''
+# Ejercicio 5
+'''
+horas= float(input ("digita el numero de horas trabajadas: "))
+costo= float(input ("digita el costo por hora: "))
+print (horas*costo )
+'''
